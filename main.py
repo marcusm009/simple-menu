d1deb55b03e1d3ae7281213b844419a9c5534f3d
Marcus Mills <marcusmills0926@gmail.com>
2018-08-08 17:49:01 -0400

    Added ability to represent Menu object as dictionary to export to JSON

--- main.py
+++ main.py
@@ -3,46 +3,26 @@ import json
 #TODO: Re-write description
 #TODO: Finish making menu class
 
-# The idea here is to make a simple menu system where you pass in a dictionary
-# using tuples as keys (zeros act as the header)
-
-# Example:
-# blueprint = {
-# (0) : "Select something: "
-# (1) : "Start"
-#     (1.0) : "Select something else: "
-#     (1.1) : "Singleplayer"
-#         (1.1.0) : "Select a difficulty: "
-#         (1.1.1) : "Easy"
-#         (1.1.2): "Hard"
-#     (1.2) : "Multiplayer"
-#         (1.1.0) : "Select a difficulty: "
-#         (1.1.1) : "Easy"
-#         (1.1.2): "Hard"
-#     (1.3) : "Options"
-# (2) : "Quit"
-#
-# menu = Menu(blueprint)
-
 class Menu:
     def __init__(self, blueprint, parent=None):
         self.parent = parent
-        self.children = []
+        self.menu_options = {}
 
         # Initialize default attributes for menu
+        self.label = ""
         self.header = "\nChoose what you would like to do:"
         self.selection_message = "\nSelection > "
         self.error_message = "Invalid selection! Please type number from list!"
 
         # Initialize attributes
-        caption = blueprint.get("caption")
+        label = blueprint.get("label")
         header = blueprint.get("head")
         selection_message = blueprint.get("selection_message")
         error_message = blueprint.get("error_message")
 
         # Try to add new attributes to menu
-        if caption is not None:
-            self.caption = caption
+        if label is not None:
+            self.label = label
         if header is not None:
             self.header = header
         if selection_message is not None:
@@ -55,40 +35,47 @@ class Menu:
 
         # Loop through blueprint with various keys to add menu options
         while True:
-            child_blueprint = blueprint.get(str(key))
+            # Assign menu_option to the specified key
+            menu_option = blueprint.get(str(key))
 
-            # Adds menu option or break out of loop
-            if child_blueprint is not None:
-                self.children.append(Menu(child_blueprint, self))
+            # If the menu option exists
+            if menu_option:
+                # Append a new menu to the menu option
+                self.menu_options[str(key)] = Menu(menu_option, self)
                 key += 1
             else:
                 break
 
-def main():
-    dict = {
-        "1": {
-            "1": {
-                "caption": "Hello"
-            },
-            "2": {
-                "caption": "Hello",
-                "function": "foo"
-            },
-            "caption": "Create new Neural Network"
-        },
-        "2": {
-            "1": {
-                "caption": "Hello"
-            },
-            "2": {
-                "caption": "Hello"
-            },
-            "caption": "Load Neural Network"
-        }
-    }
+    def as_dict(self):
+        return to_dict(self)
 
-    with open("menu.json", "w") as write_file:
-        json.dump(dict, write_file, indent=4, sort_keys=True)
+def to_dict(menu):
+    loop_attributes = ["label", "function", "header", "selection_message", "error_message"]
+    dict = {}
+
+    # Loop through different attributes and add them to the dictionary
+    for attribute in loop_attributes:
+        if hasattr(menu, attribute):
+            dict[attribute] = getattr(menu, attribute)
+
+    # Check to see if the menu contains any menu_options and recursively
+    # call to_dict to add them as a nested dictionary
+    if hasattr(menu, "menu_options"):
+        key = 1
+        menu_options = {}
+        while True:
+            try:
+                menu_options[str(key)] = to_dict(menu.menu_options[str(key)])
+                key += 1
+            except:
+                break
+        dict["menu_options"] = menu_options
+
+    return dict
+
+def main():
+    # with open("menu.json", "w") as write_file:
+    #     json.dump(dict, write_file, indent=4, sort_keys=True)
 
     with open("menu.json", "r") as read_file:
         blueprint = json.load(read_file)
@@ -96,7 +83,7 @@ def main():
     menu = Menu(blueprint)
 
     with open("object.json", "w") as write_file:
-        json.dump(menu, write_file, indent=4, sort_keys=True, default=lambda x: x.__dict__())
+        json.dump(menu.as_dict(), write_file, indent=4, sort_keys=True)
 
 
 
