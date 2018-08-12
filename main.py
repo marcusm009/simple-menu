f88afd3530f3576dc51852a2da8b84bab4d92b55
Marcus Mills <marcusmills0926@gmail.com>
2018-08-11 23:31:03 -0400

    Added a selection interface uses curses

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import curses
 
 #TODO: Re-write description
 #TODO: Add menu switching and function handling
@@ -7,97 +8,103 @@ import json
 class Menu:
     def __init__(self, blueprint, parent=None):
         self.parent = parent
-        self.menu_options = {}
+        self.menu_options = []
 
         # Initialize default attributes for menu
         self.label = ""
         self.header = "\n\tChoose what you would like to do:"
         self.separator = 50 * "="
         self.selection_message = "\nSelection > "
-        self.error_message = "Invalid selection! Please type number from list!"
+        self.function = None
 
         # Initialize attributes
         label = blueprint.get("label")
         header = blueprint.get("head")
         selection_message = blueprint.get("selection_message")
-        error_message = blueprint.get("error_message")
+        function = blueprint.get("function")
 
         # Try to add new attributes to menu
         if label is not None:
             self.label = label
         if header is not None:
             self.header = header
+        if function is not None:
+            self.function = function
         if selection_message is not None:
             self.selection_message = selection_message
-        if error_message is not None:
-            self.error_message = error_message
 
-        # Initialize key to 1
-        key = 1
-
-        # Loop through blueprint with various keys to add menu options
-        while True:
-            # Assign menu_option to the specified key
-            menu_option = blueprint.get(str(key))
-
-            # If the menu option exists
-            if menu_option:
-                # Append a new menu to the menu option
-                self.menu_options[str(key)] = Menu(menu_option, self)
-                key += 1
-            else:
-                break
+        menu_options = blueprint.get("menu_options")
+        for menu_option in menu_options:
+            self.menu_options.append(Menu(menu_option, self))
 
     def as_dict(self):
         return to_dict(self)
 
-    def show(self):
-        # Declare user input
-        inp = 0
+    def __show__(self, stdscr):
+        # Initialize option and selection
+        option = 0
         selection = None
 
+        # Initialize curses attributes
+        attributes = {}
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        attributes['normal'] = curses.color_pair(1)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        attributes['highlighted'] = curses.color_pair(2)
+
+        # Loop until an option is selected
+        while not selection:
+
+            # Clear the screen and add the header
+            stdscr.erase()
+            stdscr.addstr(self.header + '\n', curses.A_UNDERLINE)
+
+            for idx in range(len(self.menu_options)):
+                if idx == option:
+                    attr = attributes['highlighted']
+                else:
+                    attr = attributes['normal']
+
+                # Check if the menu has a parent and add an option to go back
+                # if self.parent:
+                #     stdscr.addstr("Go back\n", attr)
+
+                stdscr.addstr(self.menu_options[idx].label + '\n', attr)
+
+            # Gets the last character typed for input
+            c = stdscr.getch()
+            if c == curses.KEY_UP and option > 0:
+                option -= 1
+            elif c == curses.KEY_DOWN and option < len(self.menu_options) - 1:
+                option += 1
+            elif c == 10:
+                selection = self.menu_options[option]
+
+        function = getattr(self, "function")
+        if function is not None:
+            function()
+
+        selection.show()
+
+
+    def show(self):
         # Check to make sure there are menu options
         if not self.menu_options:
+            print("No menu options!")
             return
 
-        # Print header and separators
-        print(self.separator)
-        print(self.header)
-        print()
-        print(self.separator)
+        curses.wrapper(self.__show__)
 
-        # Check if the menu has a parent and print "go back" if it does
-        if self.parent is not None:
-            print("0. Go back")
+        # # Check if the menu has a parent and print "go back" if it does
+        # if self.parent is not None:
+        #     print("0. Go back")
+        #
+        # if inp == "0" and self.parent is not None:
+        #     self.parent.show()
 
-        # Loop through options and print them
-        key = 1
-        while True:
-            cur_option = self.menu_options.get(str(key))
-            if cur_option is None:
-                break
-            print(str(key) + ". ", end='')
-            print(cur_option.label)
-            key += 1
 
-        # Get user input and validate it using the key from the menu option
-        #   dictionary
-        while True:
-            print(self.separator, end='')
-            inp = input(self.selection_message)
-            selection = self.menu_options.get(inp)
-            if selection is not None or inp == "0":
-                break
-            else:
-                print()
-                print(self.error_message)
-
-        if inp == "0" and self.parent is not None:
-            self.parent.show()
-
-        if type(selection) is Menu:
-            selection.show()
-
+def func():
+    print("hello world!")
 
 def to_dict(menu):
     loop_attributes = ["label", "function", "header", "selection_message",
@@ -140,3 +147,25 @@ def main():
 
 
 main()
+
+# {
+#     "1": {
+#         "1": {
+#             "label": "Hello"
+#         },
+#         "2": {
+#             "label": "Hello",
+#             "function": "foo"
+#         },
+#         "label": "Create new Neural Network"
+#     },
+#     "2": {
+#         "1": {
+#             "label": "Hello"
+#         },
+#         "2": {
+#             "label": "Hello"
+#         },
+#         "label": "Load Neural Network"
+#     }
+# }
